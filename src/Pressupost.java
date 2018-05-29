0dd27175d45a43af37656ceba335e4576d1512ab
3I_10K <eliocamison@gmail.com>
2018-05-29 17:40:10 +0200

    Form budget

---
+++ src/Pressupost.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Pressupost {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton SEARCHButton;
+    private JButton NEWButton;
+    private JButton ADDITEMButton;
+    private JButton DELETEITEMButton;
+    private JTable table1;
+    private JTextArea textArea1;
+    private JButton CLOSEButton;
+    private JButton SEARCHButton1;
+    private JButton MODIFYButton;
+}
