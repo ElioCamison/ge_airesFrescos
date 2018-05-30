67939d03c13624bd2c93860ca3baf78005bf189a
3I_10K <eliocamison@gmail.com>
2018-05-30 18:16:32 +0200

    Form budget 1.2

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame jf = new JFrame(  );
+        jf.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        Pressupost pf = new Pressupost();
+        CardLayout cl = new CardLayout(  );        JPanel jp = new JPanel(  );
+        jp.add( pf.getMainPanel(),"PRESSUPUESTOS" );
+        jp.setLayout( cl );
+        jf.setContentPane( jp );
+        jf.pack();
+        jf.setVisible( true );
+    }
+}
--- src/Pressupost.java
+++ src/Pressupost.java
@@ -7,18 +7,40 @@ public class Pressupost {
     private JTextField textField2;
     private JButton SEARCHButton;
     private JButton NEWButton;
+
+    public JPanel getMainPanel() {
+        return MainPanel;
+    }
+
+    private JButton SEARCHButton1;
+    private JPanel MainPanel;
+    private JButton NEWButton1;
+    private JTable table1;
     private JButton ADDITEMButton;
     private JButton DELETEITEMButton;
-    private JTable table1;
     private JTextArea textArea1;
-    private JButton CLOSEButton;
-    private JButton SEARCHButton1;
+    private JButton SAVEButton;
+    private JButton CANCELButton;
+    private JPanel PamelButtonSaveCancel;
 
     public Pressupost() {
         NEWButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
 
+            }
+        });
+        NEWButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                NewCustomerDialog customerDialog = new NewCustomerDialog();
+                customerDialog.setModal(true);
+                customerDialog.pack();
+                customerDialog.setVisible( true );
+
+                // Dades d'un client nou
+                String userName = customerDialog.getFieldName().getText();
+                String adress   = customerDialog.getFieldAdress().getText();
             }
         });
     }
