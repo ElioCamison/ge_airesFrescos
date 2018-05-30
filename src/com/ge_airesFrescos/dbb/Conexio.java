616b59db5c28336ee18b8f59655ff0ebc2ce6e5e
3I_10K <eliocamison@gmail.com>
2018-05-30 21:37:23 +0200

    Form budget 1.4

--- src/com/ge_airesFrescos/dbb/Conexio.java
+++ src/com/ge_airesFrescos/dbb/Conexio.java
@@ -1,10 +1,17 @@
-package com.ge_airesFrescos.dbb;
-
+/*package com.ge_airesFrescos.dbb;
+import org.apache.commons.dbcp2.BasicDataSource;
 import java.sql.*;
 
 public class Conexio {
 
+    private static final java.lang.String CADENA_CONNEXIO = "jdbc:mysql://localhost/facturacio?createDatabaseIfNotExist=true&useServerPrepStmts=true";
+    private static final String USER_BBDD = "root";
+    private static final String PASS_BBDD = "test";
 
 
+    private static BasicDataSource pool;
 
-}
+    public static BasicDataSource getPool() {
+        return pool;
+    }
+}*/
