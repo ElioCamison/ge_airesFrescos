c7950ded3925f4ac87c43eb2380ce8eea9678f4c
Elio <eliocamison@gmail.com>
2018-06-04 23:23:11 +0200

    commit 2.7

--- src/com/ge_airesFrescos/Main.java
+++ src/com/ge_airesFrescos/Main.java
@@ -1,13 +1,19 @@
 package com.ge_airesFrescos;
 
+import com.ge_airesFrescos.DAO.BudgetDAO;
+import com.ge_airesFrescos.Exceptions.MySQLException;
 import com.ge_airesFrescos.GraphicEnvironment.Pressupost;
+import com.ge_airesFrescos.ImplementsDAO.BudgetImpDAO;
+import com.ge_airesFrescos.Model.Budget;
+import com.ge_airesFrescos.dbb.Conexio;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MySQLException, SQLException {
         JFrame jf = new JFrame(  );
         jf.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         Pressupost pf = new Pressupost();
