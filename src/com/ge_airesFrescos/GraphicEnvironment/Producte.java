1bfb43dc5d06017aa79e7090f650cb387904a715
3I_10K <eliocamison@gmail.com>
2018-06-06 21:42:11 +0200

    Form budget 3.6

---
+++ src/com/ge_airesFrescos/GraphicEnvironment/Producte.java
@@ -0,0 +1,10 @@
+package com.ge_airesFrescos.GraphicEnvironment;
+
+import javax.swing.*;
+
+public class Producte {
+    private JPanel panel1;
+    private JPanel ProductForm;
+    private JLabel productLabel;
+    private JButton newButton;
+}
